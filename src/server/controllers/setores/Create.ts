e98f5c3fc010f2634fb7bad3871c3f8c3688eb1f
Ewerton Lima <ewerton_rlima@hotmail.com>
2024-10-27 13:51:38 -0300

    feat: validacao de erros com yup

--- src/server/controllers/setores/Create.ts
+++ src/server/controllers/setores/Create.ts
@@ -1,12 +1,32 @@
 import { Request, Response } from "express";
+import { StatusCodes } from "http-status-codes";
+import * as yup from 'yup';
 
 interface ISetor{
-    nomeSetor: string;
+    nome_setor: string;
 }
 
-export const create = (req: Request<{}, {}, ISetor>, res: Response) => {
+const bodyValidation: yup.Schema<ISetor> = yup.object().shape({
+    nome_setor: yup.string().required().min(3),
+});
 
-    console.log(req.body  )
+export const create = async (req: Request<{}, {}, ISetor>, res: Response) => {
+    
+    let validatedData: ISetor | undefined = undefined;
 
+    try {
+        validatedData = await bodyValidation.validate(req.body)
+    } catch (error) {
+        const yupError = error as yup.ValidationError;
+        
+        return res.json({
+            error: {
+                default:yupError.message,
+            }
+        })
+    }
+
+    console.log(req.body)
+    
     return res.send('Setor criado!!');
 };
