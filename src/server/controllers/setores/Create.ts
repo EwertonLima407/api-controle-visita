dc9786caa88a5a0350458bc041ea2c9011977736
Ewerton Lima <ewerton_rlima@hotmail.com>
2024-10-28 21:48:52 -0300

    feat: Traducao do you e validacao de multiplos erros

--- src/server/controllers/setores/Create.ts
+++ src/server/controllers/setores/Create.ts
@@ -4,10 +4,12 @@ import * as yup from 'yup';
 
 interface ISetor{
     nome_setor: string;
+    sigla: string;
 }
 
 const bodyValidation: yup.Schema<ISetor> = yup.object().shape({
     nome_setor: yup.string().required().min(3),
+    sigla: yup.string().required().min(2)
 });
 
 export const create = async (req: Request<{}, {}, ISetor>, res: Response) => {
@@ -15,18 +17,20 @@ export const create = async (req: Request<{}, {}, ISetor>, res: Response) => {
     let validatedData: ISetor | undefined = undefined;
 
     try {
-        validatedData = await bodyValidation.validate(req.body)
+        validatedData = await bodyValidation.validate(req.body, {abortEarly: false})
     } catch (error) {
         const yupError = error as yup.ValidationError;
-        
-        return res.json({
-            error: {
-                default:yupError.message,
-            }
+        const errors: Record<string, string> = {}; 
+
+        yupError.inner.forEach(error => {
+            if (error.path === undefined)  return
+            errors[error.path] = error.message;
         })
+        
+        return res.status(StatusCodes.BAD_REQUEST).json({ errors })
     }
 
-    console.log(req.body)
+    console.log(validatedData)
     
     return res.send('Setor criado!!');
 };
